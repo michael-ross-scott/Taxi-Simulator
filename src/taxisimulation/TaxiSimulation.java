c3da168cf9eec60beb543efb6f243d0308c1a24d
Michael Scott <33922586+michael-ross-scott@users.noreply.github.com>
2017-11-29 17:45:31 +0200

    Took out package

--- src/taxisimulation/TaxiSimulation.java
+++ src/taxisimulation/TaxiSimulation.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package taxisimulation;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -13,7 +6,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Setup
+ * @author Michael Scott
  */
 public class TaxiSimulation {
     
