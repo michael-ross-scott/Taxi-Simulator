d09d33bc36b4d0c3d539ffa6fc12f220303993b4
Michael Scott <33922586+michael-ross-scott@users.noreply.github.com>
2017-11-29 17:43:55 +0200

    Took out package information

--- src/taxisimulation/Person.java
+++ src/taxisimulation/Person.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package taxisimulation;
-
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
- * @author Setup
+ * @author Michael Scott
  */
 public class Person extends Thread
 {
